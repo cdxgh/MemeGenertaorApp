4f39bf631f8fd8d344c184bfc2aa7fc2051f0c76
AbdulQuadir <96329368+cdxgh@users.noreply.github.com>
2023-08-26 06:16:30 +0000

    Update 8 files

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import "./styles.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Routes, Route} from 'react-router-dom';
+import HomePage from "./Pages/Home";
 
 export default function App() {
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+    <div className="container">
+      <h1>Meme Generator</h1>
+      <Routes>
+        <Route path="/" element={<HomePage/>} />
+        </Routes>
     </div>
   );
-}
+};
