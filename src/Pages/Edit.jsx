151417de2baf282bd47cb38d21b40e1399bac5dd
AbdulQuadir <96329368+cdxgh@users.noreply.github.com>
2023-08-26 08:39:52 +0000

    Update Edit.jsx

--- src/Pages/Edit.jsx
+++ src/Pages/Edit.jsx
@@ -1,4 +1,4 @@
-import { Button } from "bootstrap";
+import { Button } from "react-bootstrap";
 import Text from "../Components/Text";
 import React, { useState, createRef } from "react";
 import { useSearchParams } from "react-router-dom";
